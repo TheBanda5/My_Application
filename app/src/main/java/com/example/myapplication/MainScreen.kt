abf4a63551306fbdb90c10ee979b279aa40fe1ae
Bered Kabuluku_ST10458111 <ST10458111@vcconnect.edu.za>
2024-06-09 11:57:22 +0200

    add new modifications

--- app/src/main/java/com/example/myapplication/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/MainScreen.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+
+        val btnNavigateToDetail = findViewById<Button>(R.id.btnNavigateToDetail)
+        val btnReInputData = findViewById<Button>(R.id.btnReInputData)
+        val btnClearData = findViewById<Button>(R.id.btnClearData)
+
+        btnNavigateToDetail.setOnClickListener{
+
+        }
     }
 }
